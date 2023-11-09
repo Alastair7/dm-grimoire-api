f4f7fd1fa3a794743570b502af5addafbef34ca9
Senka <100718317+RazkuDrakon@users.noreply.github.com>
2023-11-09 16:05:28 +0100

    initiated server and first hello world

---
+++ README.md
@@ -0,0 +1,10 @@
+# DM Grimoire API
+![Grimoire](https://github.com/Alastair7/dm-grimoire-api/assets/70861797/6545d4f2-4d0f-4dd4-a958-1bcc276b15ba)
+
+## DM Grimoire
+DM Grimoire is a mobile APP designed for Dungeon & Dragons (D&D) game masters. 
+
+Think of DM Grimoire as your assistant for managing your campaigns, offering a lot of data that you can use. But that's not all, you can also randomize items, entities and encounters. Additionally, you can add your own maps and monsters to the the bestiary.
+
+## DM Grimoire API
+The API is developed with NodeJS and TypeScript, using MVC architectural pattern.
---
+++ src/routes/userRouter.ts
@@ -0,0 +1,20 @@
+import express, { Router, Request, Response } from "express";
+
+const userRouter: Router = express.Router();
+
+// This is a route for specific endpoints which is not too different from what you've seen in appRouter.
+userRouter.get("/", (req: Request, res: Response) => {
+  res.send("Hello from user router!");
+});
+
+// Here you can add more endpoints. For example:
+userRouter.get("/test", (req: Request, res: Response) => {
+  res.send("Test user route"); // If you start the server and go to http:localhost:3000/users/test
+  // You'll get that message.
+});
+
+userRouter.get("/senka", (req: Request, res: Response) => {
+  res.send("Senka test success!");
+});
+
+export default userRouter; // Don't forget to export the module to import it later in appRouter.
