75032ccd8ad020fa774f3f0b50a204a683dcad3e
Alastair7 <alexandru.ban.drmer@gmail.com>
2023-11-18 13:16:17 +0100

    ADD: ClassModels & ClassController & ClassRouter

---
+++ src/controllers/classController.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from "express";
+import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
+import { GetClassesResponse } from "../models/D&D/classes/getClassesResponseModel";
+
+export const getClasses = async (req: Request, res: Response) => {
+  try {
+    const requestConfig: AxiosRequestConfig = {
+      headers: { Accept: "application/json" },
+      timeout: 30000,
+    };
+
+    const response: AxiosResponse<GetClassesResponse> = await axios.get(
+      "https://www.dnd5eapi.co/api/classes",
+      requestConfig
+    );
+    const classesData: GetClassesResponse = {
+      count: response.data.count,
+      results: response.data.results.map((classInfo) => ({
+        index: classInfo.index,
+        name: classInfo.name,
+      })),
+    };
+
+    res.status(200).send(classesData);
+  } catch (error) {
+    res.status(400).send(`Error: ${error}`);
+  }
+};
---
+++ src/models/D&D/classes/classModel.ts
@@ -0,0 +1,4 @@
+interface Class {
+  index: string;
+  name: string;
+}
