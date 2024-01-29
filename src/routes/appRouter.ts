88c5f1e08c616736b7ad484e6cf83327c46c59b2
Alastair7 <alexandru.ban.drmer@gmail.com>
2024-01-29 21:06:02 +0100

    ADD: Equipment dependency inversion

--- src/routes/appRouter.ts
+++ src/routes/appRouter.ts
@@ -3,7 +3,7 @@ import userRouter from "./userRouter"; // Import user router
 //import classRouter from "./classRouter";
 import spellRouter from "./spellRouter";
 //import damageTypeRouter from "./damageTypeRouter";
-import equipmentRouter from "./equipmentRouter";
+//import equipmentRouter from "./equipmentRouter";
 import skillRouter from "./skillRouter";
 import magicItemRouter from "./magicItemRouter";
 //import alignmentRouter from "./alignmentRouter";
@@ -32,8 +32,8 @@ appRouter.use("/users", userRouter); // Add user router
 //appRouter.use("/classes", classRouter);
 appRouter.use("/spells", spellRouter);
 //appRouter.use("/damage-types", damageTypeRouter);
-appRouter.use("/equipment", equipmentRouter);
-appRouter.use("/proficiencies", equipmentRouter);
+//appRouter.use("/equipment", equipmentRouter);
+//appRouter.use("/proficiencies", equipmentRouter);
 appRouter.use("/skills", skillRouter);
 appRouter.use("/magic-items", magicItemRouter);
 //appRouter.use("/alignments", alignmentRouter);
--- src/services/Equipment/equipmentService.ts
+++ src/services/Equipment/equipmentService.ts
@@ -1,97 +1,32 @@
-import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
 import { GetEquipmentResponse } from "../../models/D&D/equipment/getEquipmentResponseModel";
 import { IEquipmentService } from "./iEquipmentService";
 import { GetAllEquipmentResponse } from "../../models/D&D/equipment/getAllEquipmentResponseModel";
+import { inject, injectable } from "inversify";
+import { IDndService } from "../../third-party/d&d/IDndService";
+import TYPES from "../../utils/DI/types";
+import { ApiRequestTypes } from "../../common/enums/apiRequestTypes";
 
+@injectable()
 export class EquipmentService implements IEquipmentService {
+  private _dndService: IDndService;
+
+  constructor(@inject(TYPES.IDndService) dndService: IDndService) {
+    this._dndService = dndService;
+  }
+
   async getAllEquipment(): Promise<GetAllEquipmentResponse> {
-    try {
-      const requestConfig: AxiosRequestConfig = {
-        headers: { Accept: "application/json" },
-        timeout: 30000,
-      };
+    const response: GetAllEquipmentResponse = await this._dndService.getAll(
+      ApiRequestTypes.EQUIPMENT
+    );
 
-      const response: AxiosResponse<GetAllEquipmentResponse> = await axios.get(
-        "https://www.dnd5eapi.co/api/equipment",
-        requestConfig
-      );
-
-      const equipmentData: GetAllEquipmentResponse = {
-        count: response.data.count,
-        results: response.data.results.map((equipmentInfo) => ({
-          index: equipmentInfo.index,
-          name: equipmentInfo.name,
-        })),
-      };
-
-      return Promise.resolve(equipmentData);
-    } catch (error) {
-      console.log(error);
-      throw error;
-    }
+    return response;
   }
   async getEquipment(index: string): Promise<GetEquipmentResponse> {
-    try {
-      const requestConfig: AxiosRequestConfig = {
-        headers: { Accept: "application/json" },
-        timeout: 30000,
-      };
+    const response: GetEquipmentResponse = await this._dndService.get(
+      index,
+      ApiRequestTypes.EQUIPMENT
+    );
 
-      const response: AxiosResponse<GetEquipmentResponse> = await axios.get(
-        `https://www.dnd5eapi.co/api/equipment/${index}`,
-        requestConfig
-      );
-
-      const equipmentData: GetEquipmentResponse = {
-        desc: response.data.desc,
-        index: response.data.index,
-        name: response.data.name,
-        equipment_category: {
-          index: response.data.equipment_category?.index,
-          name: response.data.equipment_category?.name,
-        },
-        weapon_category: response.data.weapon_category,
-        weapon_range: response.data.weapon_range,
-        category_range: response.data.category_range,
-        cost: {
-          quantity: response.data.cost?.quantity,
-          unit: response.data.cost?.unit,
-        },
-        damage: {
-          damage_dice: response.data.damage?.damage_dice,
-          damage_type: {
-            index: response.data.damage?.damage_type?.index,
-            name: response.data.damage?.damage_type?.name,
-            url: response.data.damage?.damage_type?.url,
-          },
-        },
-        range: {
-          normal: response.data.range?.normal,
-          long: response.data.range?.long,
-        },
-        weight: response.data.weight,
-        properties: response.data.properties?.map((propertyInfo) => ({
-          index: propertyInfo.index,
-          name: propertyInfo.name,
-        })),
-        two_handed_damage: {
-          damage_dice: response.data.two_handed_damage?.damage_dice,
-          damage_type: {
-            index: response.data.two_handed_damage?.damage_type?.index,
-            name: response.data.two_handed_damage?.damage_type?.name,
-            url: response.data.two_handed_damage?.damage_type?.url,
-          },
-        },
-        contents: response.data.contents?.map((contentInfo) => ({
-          index: contentInfo?.index,
-          name: contentInfo?.name,
-        })),
-      };
-
-      return Promise.resolve(equipmentData);
-    } catch (error) {
-      console.log(error);
-      throw error;
-    }
+    return response;
   }
 }
